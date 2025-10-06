0f17ba690c4bc5c01093f4a5372697f768c3b638
Teerapat Ananpaisansin <123724378+TeerapatAnanpaisansin@users.noreply.github.com>
2025-10-06 10:58:26 +0700

    Delete api/debug directory

--- api/debug/env.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const keys = [
-    "GRIST_BASE_URL","GRIST_API_KEY","GRIST_DOC_ID",
-    "USERS_TABLE","LINKS_TABLE","JWT_SECRET","ADMIN_EMAILS",
-  ];
-  const seen = Object.fromEntries(keys.map(k => [k, !!(process.env[k] && String(process.env[k]).trim())]));
-  res.status(200).json({ ok: true, seen });
-}
